048188484ac5ce29daca2568c1d7b7ce732ad7af
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:52:48 -0300

    firts dev1

--- src/br/fai/teste3/Main.java
+++ src/br/fai/teste3/Main.java
@@ -1,5 +1,22 @@
 package br.fai.teste3;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+
+
+	}
+	
+	Scanner scanner = new Scanner(System.in);
+
+
+	private void start() {
+		System.out.println("Hello");
+	}
+
 
 }
